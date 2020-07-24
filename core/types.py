ec5f8ce4e21510f75ba6f7e5da2de68e7b8e6236
Marcelo Elizeche Landó <melizeche@gmail.com>
2020-07-24 11:23:46 -0400

    Use RSA private/public key for Actor key

--- core/types.py
+++ core/types.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
 from blake3 import blake3
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 from dataclasses import dataclass
 
 from typing import Any, List
@@ -22,11 +25,28 @@ class Block:
         key.update(str(self.previous_hash).encode('utf-8'))
         return key.hexdigest()
 
-class Actor:
+class Actor: # Wallet?
     def __init__(self, name, actor_type):
         self.name = name
-        self.key = blake3(str(self.name).encode('utf-8')).hexdigest()
+        self.key = self.generate_key()
         self.type = actor_type
+
+    def generate_key(self):
+        return rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+
+    @property
+    def public_key(self):
+        pk = self.key.public_key()
+        pem = pk.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        return pem
+
     
     def __repr__(self):
         return f'<{self.name}: {self.key}>'
